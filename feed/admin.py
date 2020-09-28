31f3722e3fd7372e9d97a6e0ffc77afcd07a2ce5
sarthak712 <sarthaksrivastavadtu712@gmail.com>
2020-09-28 21:36:30 +0530

    admin

--- feed/admin.py
+++ feed/admin.py
@@ -3,3 +3,4 @@ from feed.models import FriendRequest
 # Register your models here.
 
 admin.site.register(FriendRequest)
+# admin.site.register(Fr)
--- post/admin.py
+++ post/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from post.models import Post, TagNotification
+from post.models import Post, TagNotification, HashTags, HashTagsPostTable, Like, Dislike, TaggedPost
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(TagNotification)
+admin.site.register(Like)
+admin.site.register(Dislike)
+admin.site.register(HashTagsPostTable)
+admin.site.register(HashTags)
+admin.site.register(TaggedPost)
