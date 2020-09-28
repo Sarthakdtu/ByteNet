1d15cfea0a38867809a606e570783665ca6f4d47
sarthak712 <sarthaksrivastavadtu712@gmail.com>
2020-09-28 10:46:34 +0530

    register model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from accounts.models import UserProfileInfo, User
+from accounts.models import UserProfileInfo, User, Friend
 # Register your models here.
 
 admin.site.register(UserProfileInfo)
+admin.site.register(Friend)
