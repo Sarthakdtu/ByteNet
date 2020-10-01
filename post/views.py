241488545e9f99b26a60ab373ff2160e2af53ed5
sarthak712 <sarthaksrivastavadtu712@gmail.com>
2020-10-01 18:49:10 +0530

    diabled link preview

--- post/views.py
+++ post/views.py
@@ -75,17 +75,17 @@ def create_post(request):
             # print("Got the file")
         except Exception as e:
             print(e)
-        try:
-            article_url = request.POST['article_url']
-            print(article_url)
-            if article_url:
-                article_preview = get_link_preview(article_url)
-                if article_preview:
-                    post.article_link = article_url
-                    post.article_preview = article_preview
-        except Exception as e:
-            print(e)
-        post.save()
+        # try:
+        #     article_url = request.POST['article_url']
+        #     print(article_url)
+        #     if article_url:
+        #         article_preview = get_link_preview(article_url)
+        #         if article_preview:
+        #             post.article_link = article_url
+        #             post.article_preview = article_preview
+        # except Exception as e:
+        #     print(e)
+        # post.save()
         for friend in tagged_friends:
             tagged_friend = UserProfileInfo.objects.get(user__username=friend)
             _ = TaggedPost.objects.create(post=post, user=tagged_friend)
