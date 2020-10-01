7b5e1157313af68b52ef103b38364dd79afa36fb
sarthak712 <sarthaksrivastavadtu712@gmail.com>
2020-10-01 13:40:16 +0530

    friend bot and few other changes

--- scripts/god_script.py
+++ scripts/god_script.py
@@ -4,7 +4,7 @@ from scripts.friend_bot import create_friends
 from scripts.like_dislike_function_bots import like_dislike
 
 
-# create_friends()
-# like_dislike()
-# create_bot_users()
+create_friends()
+like_dislike()
+create_bot_users()
 create_bot_posts()
