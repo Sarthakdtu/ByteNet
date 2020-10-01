1b98f2d8a8f63576ab42948b9bd1283021aa2037
sarthak712 <sarthaksrivastavadtu712@gmail.com>
2020-10-01 12:12:39 +0530

    like_dislike_bot

---
+++ scripts/like_dislike_function_bots.py
@@ -0,0 +1,25 @@
+from post.models import Like, Dislike, Post
+from accounts.models import UserProfileInfo
+from scripts.probability_generator import get_prob
+import random
+
+users = list(UserProfileInfo.objects.filter(is_bot=True))
+posts = Post.objects.filter(author_profile__is_bot=True)
+
+for post in posts:
+    user = random.choice(users)
+    if Like.objects.filter(post=post, user=user).exists() or Dislike.objects.filter(post=post, user=user):
+        print("Moving On")
+        continue
+    if not get_prob():
+        _ = Like.objects.get_or_create(user=user, post=post)
+        post.num_likes += 1
+        post.save()
+        print("Liked")
+    else:
+        if get_prob():
+            if not get_prob():
+                _ = Dislike.objects.get_or_create(user=user, post=post)
+                post.num_dislikes +=1
+                post.save()
+                print("Disliked")
