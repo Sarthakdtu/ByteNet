925eafbcec1c294be5d4b5ed5946df009f5bdf3c
sarthak712 <sarthaksrivastavadtu712@gmail.com>
2020-10-03 00:25:38 +0530

    god task

--- scripts/god_task.py
+++ scripts/god_task.py
@@ -4,15 +4,8 @@ from scripts.friend_bot import create_friends
 from scripts.like_dislike_function_bots import like_dislike
 from scripts.trending_script import trending
 
-def get_this_bread():
-    res = "Done"
-    try:
-        create_bot_users()
-        create_bot_posts()
-        create_friends()
-        like_dislike()
-        trending()
-    except Exception as e:
-        print("Error", e)
-        res = str(e)
-    return res
+create_bot_users()
+create_bot_posts()
+create_friends()
+like_dislike()
+trending()
